f381697125ede2e034ea7a7a0fcb0b8b97028489
Allen Johnson <webequate@gmail.com>
2023-05-22 19:51:42 -0600

    Fix homepage layout on non-mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const HomePage: NextPage<HomePageProps> = ({
         transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
       >
         <div className="flex flex-col-reverse lg:flex-row">
-          <div className="w-full lg:w-1/2 mb-10 lg:mb-0 md:mr-6">
+          <div className="w-full lg:w-1/2 mb-10 lg:mb-8 md:mr-6">
             <h1 className="text-4xl sm:text-5xl font-bold text-dark-1 dark:text-light-1 mb-4 uppercase">
               {name}
             </h1>
@@ -71,12 +71,12 @@ const HomePage: NextPage<HomePageProps> = ({
             </div>
           </div>
 
-          <div className="w-full lg:w-1/2 mb-10 lg:mb-0 md:ml-6">
+          <div className="w-full lg:w-1/2 mb-10 lg:mb-0">
             <motion.div
               initial={{ opacity: 0, y: -180 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
-              className="sm:ml-10"
+              className="lg:ml-10"
             >
               <ThemedImage />
             </motion.div>
