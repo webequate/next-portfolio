e3bbc41d8d0714d8b4011879d2baa86a7c6ce23e
Allen Johnson <webequate@gmail.com>
2024-12-29 15:49:43 -0700

    Add next-sitemap and generate sitemap.xml and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: "https://portfolio.webequate.com",
+  exclude: ["/featured", "/featured/**", "/testimonials"],
+  generateRobotsTxt: true,
+};
