368628b6693ec3aabb281638d66955dfd97bd8be
Allen Johnson <webequate@gmail.com>
2025-08-04 03:08:10 -0600

    Fix up URLs and sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // next-sitemap.config.js
 module.exports = {
-  siteUrl: "https://portfolio.webequate.com",
+  siteUrl: "https://webequate.com",
   exclude: ["/featured", "/featured/**", "/testimonials"],
   generateRobotsTxt: true,
 };
