e22b9213205277b83e97b6c20e0e0d6794ccae20
Allen Johnson <webequate@gmail.com>
2026-01-07 01:59:00 -0700

    Clean up sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,17 @@ module.exports = {
   siteUrl: "https://webequate.com",
   exclude: ["/featured", "/featured/**", "/testimonials"],
   generateRobotsTxt: true,
+  changefreq: undefined,
+  priority: undefined,
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      lastmod: new Date().toISOString(),
+    };
+  },
+  additionalPaths: async (config) => {
+    const result = [];
+    return result;
+  },
+  sortEntries: true,
 };
